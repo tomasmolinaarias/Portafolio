4d86af9f8a06ae85ee3f164acce20e36e0c188cd
victor molina <tomasmolinaarias12@gmail.com>
2023-04-25 02:22:53 -0400

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../Style/Home.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import cv from "../assets/document/CV.pdf";
+import cv from "../assets/document/CV_new.pdf";
 export const Home = () => {
   return (
     <div className="home">
